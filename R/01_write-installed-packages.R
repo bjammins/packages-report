22e094d08ba9624ff41b47740922d132d41535f2
Benjamin Scheich <benjmain.scheich@gmail.com>
2018-02-01 14:48:06 -0500

    first project commit

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,15 +1,20 @@
 ## deja vu from yesterday!
+library(fs)
+library(tidyverse)
+library(here)
 
 ## create a data frame of your installed packages
-
+ipt <- installed.packages() %>%
+  as_tibble()
 ## keep the variables
 ##   * Package
 ##   * LibPath
 ##   * Version
 ##   * Priority
 ##   * Built
-
+ipt2 <- ipt %>% select(Package, LibPath, Version, Priority, Built)
 ## write it to data/installed-packages.csv
+write.csv(ipt2, file=here("data", "installed-packages.csv"))
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
